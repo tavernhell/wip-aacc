77d01f5e3ed49380af23f94403c62987c6327f15
n00bCod3r <n00bCod3r@protonmail.com>
2021-02-13 17:13:03 +0100

    minor improvements 2
    new grid elements added
    new gorm controls added
    modified CSS and JS for new controls

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
 //! GLOBAL VARIABLES
-const imagesizeGutterContainer = document.querySelector('#imgsize-gutter-container');
+const optionsContainer = document.querySelector('#options-container');
+const chartTitle = document.querySelector('#chart-title');
+const chartTitleInput = document.querySelector('#chart-title-input');
+const chart = document.querySelector('#chart');
 const root = document.documentElement;
 
-
 //! EVENT LISTENERS
-imagesizeGutterContainer.addEventListener('input', (e) => {
+optionsContainer.addEventListener('input', (e) => {
   const elem = e.target;
+  //img size
   if (elem.matches('#img-size')) {
-    chartFunctions.changeImgSize(elem);
+    options.changeImgSize(elem);
   }
+  //gutter
   if (elem.matches('#gutter')) {
-    chartFunctions.changeGutter(elem);
+    options.changeGutter(elem);
+  }
+  //input for image url
+  if (elem.matches('#bg-img-url-input')) {
+    options.changeBgImage(elem);
+  }
+  //repeat bg checkbox
+  if (elem.matches('#bg-repeat-chk')) {
+    options.repeatBgImage();
+  }
+  //title input
+  if (elem.matches('#chart-title-input')) {
+    options.modifyChartTitle(elem);
   }
 });
 
 
 //! FUNCTIONS
-const chartFunctions = {
-  changeBgColorVar() {
-    //targets the css variable
-    root.style.setProperty('--chart-bg-color', this.toHEXString());
+const options = {
+  changeBgColor() {
+    chart.style.backgroundColor = this.toHEXString();
   },
 
   changeImgSize(elem) {
@@ -30,6 +45,19 @@ const chartFunctions = {
   changeGutter(elem) {
     //TODO set of CSS variables
     document.querySelector('#gutter-span').textContent = elem.value;
+  },
+
+  changeBgImage(elem) {
+    const url = elem.value;
+    chart.style.backgroundImage = `url(${url})`;
+  },
+
+  modifyChartTitle(elem) {
+    chartTitle.textContent = elem.value;
+  },
+
+  repeatBgImage() {
+    chart.classList.toggle('repeat-bg');
   }
 }
 
@@ -45,7 +73,7 @@ jscolor.presets.default = {
 //specific options
 const bgColorPicker = new JSColor('#bg-color-btn', { 
   value: '#000',
-  onInput: chartFunctions.changeBgColorVar,
+  onInput: options.changeBgColor,
 });
 
 const textColorPicker = new JSColor('#text-color-btn', { 
